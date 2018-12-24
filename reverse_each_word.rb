743e55ed93f235a9f0102012168a33c58328c3f8
Kyle Houghton <kylebenz@hotmail.com>
2018-12-24 15:42:38 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,10 @@
+sentence1 = "Hey guys this is a test to reverse"
 
 def reverse_each_word(sentence1)
   to_a(sentence1)
   sentence1.collect {|word| word.reverse}
   sentence1.join
 end
+
+reverse_each_word(sentence1)
+
