653ca35a8770ec7f79b4a95b7fb398ada04123e6
Kyle Houghton <kylebenz@hotmail.com>
2018-12-24 16:13:02 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,8 +1,13 @@
 #sentence1 = "Hey guys this is a test to reverse"
 
+#def reverse_each_word(sentence1)
+#  sentence1 = sentence1.split.collect {|word| word.reverse}
+#  sentence1.join(" ")
+#end
+
 def reverse_each_word(sentence1)
-  sentence1 = sentence1.split.collect {|word| word.reverse}
-  sentence1.join(" ")
+  sentence2 = sentence1.split.each {|word| push(word.reverse)}
+  sentence2.join(" ")
 end
 
 #reverse_each_word(sentence1)
